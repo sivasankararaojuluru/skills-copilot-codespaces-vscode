ed3865a37974f17f63bb59656c27bb4e953e172a
Sivasankararao Juluru <juluru@gmail.com>
2023-07-19 13:36:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,55 @@
+function skilsMember() {
+    var skilsMember = [
+        {
+            "id": 1,
+            "name": "HTML",
+            "percent": 90
+        },
+        {
+            "id": 2,
+            "name": "CSS",
+            "percent": 80
+        },
+        {
+            "id": 3,
+            "name": "Javascript",
+            "percent": 70
+        },
+        {
+            "id": 4,
+            "name": "PHP",
+            "percent": 60
+        },
+        {
+            "id": 5,
+            "name": "MySQL",
+            "percent": 50
+        },
+        {
+            "id": 6,
+            "name": "Python",
+            "percent": 40
+        },
+        {
+            "id": 7,
+            "name": "Java",
+            "percent": 30
+        },
+        {
+            "id": 8,
+            "name": "C++",
+            "percent": 20
+        },
+        {
+            "id": 9,
+            "name": "C#",
+            "percent": 10
+        },
+        {
+            "id": 10,
+            "name": "Ruby",
+            "percent": 0
+        }
+    ];
+    return skilsMember;
+}
